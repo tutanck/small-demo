50be933f0082a329020598505a8ba58ae6641235
tutanck <tutanck@gmail.com>
2024-11-04 22:54:27 +0100

    _

--- src/index.js
+++ src/index.js
@@ -1,21 +1,71 @@
 import smallapi from 'smallapi-js';
+import {
+  uniqueNamesGenerator,
+  /* Config, */ names,
+} from 'unique-names-generator';
+
+const config /* : Config */ = {
+  dictionaries: [names],
+};
 
 const api = await smallapi(
-  //   'https://desolate-thicket-04809-1d851952fc60.herokuapp.com/',
-  'http://localhost:7679/',
+  'https://desolate-thicket-04809-1d851952fc60.herokuapp.com/',
   {
-    // apiKey: 'my-secret-key',
-    debug: true,
+    apiKey: 'HDRTmMSq-GKARxduy-PcPywRnT-HANrf2Jn',
+    debug: !true,
   },
 );
 
 console.log({ api });
 
-const user = await api.createUser({
-  firstName: 'John',
-  lastName: 'Doe',
-  email: 'john.doe@mail.com',
-  age: 32,
-});
+const usersCount = await api.countUser();
 
-console.log({ user });
+console.log({ usersCount });
+
+if (usersCount > 0) {
+  const users = await api.findUserByQuery();
+
+  console.log({ users });
+}
+
+if (usersCount < 3) {
+  const firstName = uniqueNamesGenerator(config);
+  const lastName = uniqueNamesGenerator(config);
+
+  console.log({ firstName, lastName });
+
+  const createdUser = await api.createUser({
+    firstName,
+    lastName,
+    email: `${firstName}.${lastName}@email.com`,
+    age: 0,
+  });
+
+  console.log({ createdUser });
+
+  const users = await api.findUserByQuery();
+
+  console.log({ users });
+
+  const usersCount = await api.countUser();
+
+  console.log({ usersCount });
+}
+
+await api.updateUserByQuery({ age: { $lt: 7 } }, { $inc: { age: 1 } });
+console.log('\nAll existing users got older with 1 year...\n');
+
+const users = await api.findUserByQuery();
+
+console.log({ users });
+
+await api.removeUserByQuery({ age: { $gte: 7 } });
+console.log('\nAll users over 6 got removed...\n');
+
+const remainingUsers = await api.findUserByQuery();
+
+console.log({ remainingUsers });
+
+const remainingUsersCount = await api.countUser();
+
+console.log({ remainingUsersCount });
