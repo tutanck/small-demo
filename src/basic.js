a182c53d6f2da8f6eda8af20d9f28c045b91d2de
tutanck <tutanck@gmail.com>
2024-12-16 18:44:28 +0100

    _

--- src/basic.js
+++ src/basic.js
@@ -1,9 +1,4 @@
 import { smallapi } from 'smallapi-js';
-import { uniqueNamesGenerator, names } from 'unique-names-generator';
-
-const config = {
-  dictionaries: [names],
-};
 
 const api = await smallapi(process.env.API_URL, {
   apiKey: process.env.API_KEY,
@@ -11,54 +6,15 @@ const api = await smallapi(process.env.API_URL, {
 
 console.log({ api });
 
-const usersCount = await api.countUserDocuments();
+const createdUser = await api.createUser({
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'John.Doe@email.com',
+  age: 33,
+});
 
-console.log({ usersCount });
-
-if (usersCount > 0) {
-  const users = await api.findUserByQuery();
-
-  console.log({ users });
-}
-
-if (usersCount < 3) {
-  const firstName = uniqueNamesGenerator(config);
-  const lastName = uniqueNamesGenerator(config);
-
-  console.log({ firstName, lastName });
-
-  const createdUser = await api.createUser({
-    firstName,
-    lastName,
-    email: `${firstName}.${lastName}@email.com`,
-    age: 0,
-  });
-
-  console.log({ createdUser });
-
-  const users = await api.findUserByQuery();
-
-  console.log({ users });
-
-  const usersCount = await api.countUserDocuments();
-
-  console.log({ usersCount });
-}
-
-await api.updateUserByQuery({ age: { $lt: 7 } }, { $inc: { age: 1 } });
-console.log('\nAll existing users got older with 1 year...\n');
+console.log({ createdUser });
 
 const users = await api.findUserByQuery();
 
 console.log({ users });
-
-await api.removeUserByQuery({ age: { $gte: 7 } });
-console.log('\nAll users over 6 got removed...\n');
-
-const remainingUsers = await api.findUserByQuery();
-
-console.log({ remainingUsers });
-
-const remainingUsersCount = await api.countUserDocuments();
-
-console.log({ remainingUsersCount });
---
+++ src/population-growth.js
@@ -0,0 +1,64 @@
+import { smallapi } from 'smallapi-js';
+import { uniqueNamesGenerator, names } from 'unique-names-generator';
+
+const config = {
+  dictionaries: [names],
+};
+
+const api = await smallapi(process.env.API_URL, {
+  apiKey: process.env.API_KEY,
+});
+
+console.log({ api });
+
+const usersCount = await api.countUserDocuments();
+
+console.log({ usersCount });
+
+if (usersCount > 0) {
+  const users = await api.findUserByQuery();
+
+  console.log({ users });
+}
+
+if (usersCount < 3) {
+  const firstName = uniqueNamesGenerator(config);
+  const lastName = uniqueNamesGenerator(config);
+
+  console.log({ firstName, lastName });
+
+  const createdUser = await api.createUser({
+    firstName,
+    lastName,
+    email: `${firstName}.${lastName}@email.com`,
+    age: 0,
+  });
+
+  console.log({ createdUser });
+
+  const users = await api.findUserByQuery();
+
+  console.log({ users });
+
+  const usersCount = await api.countUserDocuments();
+
+  console.log({ usersCount });
+}
+
+await api.updateUserByQuery({ age: { $lt: 7 } }, { $inc: { age: 1 } });
+console.log('\nAll existing users got older with 1 year...\n');
+
+const users = await api.findUserByQuery();
+
+console.log({ users });
+
+await api.removeUserByQuery({ age: { $gte: 7 } });
+console.log('\nAll users over 6 got removed...\n');
+
+const remainingUsers = await api.findUserByQuery();
+
+console.log({ remainingUsers });
+
+const remainingUsersCount = await api.countUserDocuments();
+
+console.log({ remainingUsersCount });
