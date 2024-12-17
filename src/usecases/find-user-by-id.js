ac5880a13139f96b87ad2d8b735bbfb2aef557bf
tutanck <tutanck@gmail.com>
2024-12-17 23:57:21 +0100

    _

--- src/usecases/find-user-by-id.js
+++ src/usecases/find-user-by-id.js
@@ -9,23 +9,23 @@ const api = await smallapi(process.env.API_URL, {
   apiKey: process.env.API_KEY,
 });
 
-console.log({ api });
+console.log('api:', api, '\n');
 
 const usersCount = await api.countUserDocuments();
 
-console.log({ usersCount });
+console.log('usersCount:', usersCount, '\n');
 
 if (usersCount > 0) {
   const users = await api.findUserByQuery();
 
-  console.log({ users });
+  console.log('users:', users, '\n');
 }
 
 if (usersCount < 3) {
   const firstName = uniqueNamesGenerator(config);
   const lastName = uniqueNamesGenerator(config);
 
-  console.log({ firstName, lastName });
+  console.log('firstName:', firstName, lastName, '\n');
 
   const createdUser = await api.createUser({
     firstName,
@@ -34,15 +34,15 @@ if (usersCount < 3) {
     age: 0,
   });
 
-  console.log({ createdUser });
+  console.log('createdUser:', createdUser, '\n');
 
   const users = await api.findUserByQuery();
 
-  console.log({ users });
+  console.log('users:', users, '\n');
 
   const usersCount = await api.countUserDocuments();
 
-  console.log({ usersCount });
+  console.log('usersCount:', usersCount, '\n');
 }
 
 await api.updateUserByQuery({ age: { $lt: 7 } }, { $inc: { age: 1 } });
@@ -50,15 +50,15 @@ console.log('\nAll existing users got older with 1 year...\n');
 
 const users = await api.findUserByQuery();
 
-console.log({ users });
+console.log('users:', users, '\n');
 
 await api.removeUserByQuery({ age: { $gte: 7 } });
 console.log('\nAll users over 6 got removed...\n');
 
 const remainingUsers = await api.findUserByQuery();
 
-console.log({ remainingUsers });
+console.log('remainingUsers:', remainingUsers, '\n');
 
 const remainingUsersCount = await api.countUserDocuments();
 
-console.log({ remainingUsersCount });
+console.log('remainingUsersCount:', remainingUsersCount, '\n');
---
+++ src/utils/util.js
@@ -0,0 +1,5 @@
+export function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
